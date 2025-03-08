78b39051483b3c3c0ca4395bad5dbe729a4265a6
Sam Kalt <sam.kalt@icloud.com>
2025-03-08 15:30:04 -0700

    add comments describing functionality

--- trip-generator.js
+++ trip-generator.js
@@ -1,6 +1,6 @@
-/* This function will generate a skywest trip for the day
+/* This function will generate a skywest trip for one day
 
-Varibles:
+Output :
 Aircraft type you will be flying
 Your crew position
 Number of legs you will be flying
@@ -9,18 +9,26 @@ Your route - airports you will be flying to
 */
 
 function generateTrip() {
+
+    // Data banks
     const aircraft = ['ERJ', 'CRJ 700', 'CRJ 550', 'CRJ 200']
     const position = ['Captain', 'First Officer', 'Flight Attendant']
     const airports = ['APA', 'COS', 'GJT', 'ASE', 'DRO', 'PUB', 'LAR', 'CPR', 'COD']
+
+    // Roll for how many flights the trip will have
     let legs = Math.ceil((Math.random() * 4) + 1)
 
+    // Function to create a route for the trip in a list
     function generateAirports() {
         let list = ['DEN', '>>>']
         for (i = 0; i < legs - 1; i++) {
             let nextStop = Math.floor(Math.random() * airports.length)
+
+            // Validate next destination is not the same as the previous destination
             while (airports[nextStop] === list[list.length - 2]) {
                 nextStop = Math.floor(Math.random() * airports.length)
             }
+
             list.push(airports[nextStop])
             list.push('>>>')
         }
@@ -28,11 +36,14 @@ function generateTrip() {
         return list
     }
 
+    // Roll for aircraft type and position
     let aircraftIndex = Math.floor(Math.random() * aircraft.length)
     let positionIndex = Math.floor(Math.random() * position.length)
 
+    // Create the route
     let airportsList = generateAirports()
 
+    // Log results
     console.log(`You got assigned a trip!`)
     console.log(`You will be flying on the ${aircraft[aircraftIndex]}.`)
     console.log(`You will be operating as the ${position[positionIndex]}.`)
